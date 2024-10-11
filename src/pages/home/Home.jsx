187139b8030369f9a9ac08d4ae809819453f21ad
azadvt <azad.vt@outlook.com>
2024-10-11 23:39:18 +0530

    home updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from '../../components/Banner'
+// import Banner from '../../components/Banner'
 import Catagories from './Catagories'
 import SpecialDishes from './SpecialDishes'
 import Testimonials from './Testimonials'
@@ -7,17 +7,26 @@ import OurServices from './OurServices'
 import ParallaxLandingPage from './parallaxLandingPage'
 import Learnmore_Become_Affiliate from './Learnmore_Become_Affiliate'
 import Best_Sellers from './Best_Sellers'
+import Banner from '../../components/banner/Banner'
+import CardSection from '../../components/cardSection/CardSection'
+import SideScrollSection from '../../components/sideScrollSection/SideScrollSection'
+import SubscriptionOptions from '../../components/subscriptionOptions/SubscriptionOptions'
+import SocialAppSection from '../../components/socialAppSection/SocialAppSection'
 
 const Home = () => {
   return (
     <div>
        <Banner/>
-       <Catagories/>
+       <CardSection/>
+       <SideScrollSection/>
+       <SubscriptionOptions/>
+       <SocialAppSection/>
+       {/* <Catagories/> */}
        {/* <Best_Sellers/> */}
        {/* <SpecialDishes/> */}
-       <Learnmore_Become_Affiliate/>
+       {/* <Learnmore_Become_Affiliate/> */}
        {/* <Testimonials/> */}
-       <OurServices/>
+       {/* <OurServices/> */}
        {/* <ParallaxLandingPage/> */}
     
     </div>
