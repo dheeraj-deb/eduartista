59e55e2785c78b939ca57cbfe9e37aea6a5d2924
azadvt <azad.vt@outlook.com>
2024-10-11 23:39:38 +0530

    main updated with new footer and nav

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import "../../src/App.css";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import { AuthContext } from "../contexts/AuthProvider";
 import LoadingSpinner from "../components/LoadingSpinner";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 
 export const loginContext = createContext();
