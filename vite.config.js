89c30d21d14b1983ad575512d89b42f98aad0116
Muhsirkhan123 <muhsir.khan@maisondauraine.com>
2024-10-12 00:20:53 +0530

    vite.onfig

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@fortawesome/fontawesome-svg-core"],
+      // external: ["@fortawesome/fontawesome-svg-core"],
     },
   },
 });
